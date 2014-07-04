9e966d65cd0f02d80d02b14f5fdc0ee03e978550
x13g020 <x13g020@PC205N007.chiba-fjb.ac.jp>
2014-07-04 19:01:41 +0900

    aaaa

--- kadai/src/kadai001/CharaData.java
+++ kadai/src/kadai001/CharaData.java
@@ -4,38 +4,37 @@ import java.io.*;
 import java.util.Scanner;
 
 public class CharaData {
-	int HP, EXP, MONEY;
-	String name;
+	static int HP, EXP, MONEY;
+	static String name;
 
 	public static void main(String[] args) {
+
 		Scanner s = new Scanner(System.in);
 		Scanner hp = new Scanner(System.in);
 		Scanner exp = new Scanner(System.in);
 		Scanner money = new Scanner(System.in);
-		
+		Scanner Y = new Scanner(System.in);
+		Scanner n = new Scanner(System.in);
+
 		System.out.println("名前を入力してください");
-		String name =s.next();
+		name = s.next();
+
 		System.out.println("スターテスを入力してください");
 		System.out.print("HP:");
-		int HP = hp.nextInt();
+		HP = hp.nextInt();
+
 		System.out.print("EXP:");
-		int EXP = exp.nextInt();
+		EXP = exp.nextInt();
+
 		System.out.print("MONEY:");
-		int MONEY = money.nextInt();
+		MONEY = money.nextInt();
 		
-		
-		
-		try{
-		File saveFile = new File("i:\\\\savedata.txt");
-		BufferedWriter br = new BufferedWriter(new FileWriter(saveFile));
-		
-		
-		}catch(FileNotFoundException e){
-			System.out.println(e);
-		}catch(IOException e){
-			System.out.println(e);
+		System.out.println("ロードしますか？[y/n]");
+		String y = Y.next();
+		if(y.equals(Y)){
+			LoadData l = new LoadData();
+			System.out.println(l);
 		}
-	
 
 	}
 }
